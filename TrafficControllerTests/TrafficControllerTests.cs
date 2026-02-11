63efd5c27897d7d35e020166a97379ce3df07aa0
AKavaa <alex.kavaleuskiy33@gmail.com>
2026-02-11 14:44:52 +0200

    add methods

---
+++ TrafficControllerTests/TrafficControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+
+
+[TestFixture]
+class TrafficControllerTests
+{
+    // This test checks if the project template is correctly set up on your machine.
+    // Uncomment the test method below and verify that it appears in the Test Explorer (Test -> Test Explorer).
+    // Running the unit test should pass with the message "Example Test Passed."
+    // If the test is not visible, doesn't run, or fails: ask a tutor for assistance.
+    // Once you confirm the template runs unit tests successfully, you can delete this test and comment.
+
+    //[Test]
+    //public void TemplateTest()
+    //{
+    //    Assert.Pass("Example Test Passed!");
+    //}
+
+
+    //use the following naming convention for your test method names MethodBeingTested_TestScenario_ExpectedOutput
+    //E.g. SetCurrentState_InvalidState_ReturnsFalse
+
+    //Write Test Code Here...
+
+}
