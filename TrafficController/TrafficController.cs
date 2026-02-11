c369cccb7a304a49b5b623f8e313cb33141cdbc7
AKavaa <alex.kavaleuskiy33@gmail.com>
2026-02-11 14:48:14 +0200

    add methods

---
+++ TrafficController/TrafficController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SmartTrafficController
+{
+
+
+    public class TrafficController
+    {
+
+        public int GetIntersectionID(int intersectionID)
+        {
+            return intersectionID;
+        }
+
+        public int GetCurrentVehicleSignalState(int CurrentVehicleSignalState)
+        {
+            return CurrentVehicleSignalState;
+        }
+
+    }
+}
